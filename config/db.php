5a39484c7b2a4fb54837a28d7c8d689be3304f11
Matheus Ribeiro <matheusribeirotine@gmail.com>
2025-11-28 18:44:52 -0300

    Update database configuration for infinityfree

--- config/db.php
+++ config/db.php
@@ -1,19 +1,19 @@
 <?php
 // config/db.php
 
-$host = 'localhost';
-$dbname = 'tasker_db';
-$username = 'root'; // Padrão do XAMPP
-$password = '';     // Padrão do XAMPP (vazio)
+$host = 'sql207.infinityfree.com';
+$dbname = 'if0_40547603_tasker';
+$username = 'if0_40547603';
+$password = 'koguAvA1yTU';
 
 try {
-    // Criação da conexão usando PDO (mais seguro)
+    // Criação da conexão usando PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
     // Configura para lançar erros em caso de falha
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
-    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+    die("Erro na conexão com o banco de dados. Verifique as credenciais.");
 }
 ?>
